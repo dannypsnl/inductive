4a579501306c5d5a20cff2815ba49afc00ec37bf
dannypsnl <dannypsnl@gmail.com>
2020-06-19 11:09:25 +0800

    ind: env

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection "inductive")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define pkg-desc "Inductive")
+(define version "0.0")
+(define pkg-authors '(dannypsnl))
