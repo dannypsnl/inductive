ef7a0bb980e349ce5df91c934bf8780f78c54755
dannypsnl <dannypsnl@gmail.com>
2020-06-19 11:17:36 +0800

    ind: record position for error reporting

--- syntax.rkt
+++ syntax.rkt
@@ -1,14 +1,17 @@
 #lang typed/racket
 
-(struct syntax:inductive
+(struct syntax ([pos : srcloc]) #:transparent)
+
+(struct syntax:inductive syntax
   ([name : String]
    [constructors : (List syntax:constructor)])
   #:transparent)
-(struct syntax:constructor
+(struct syntax:constructor syntax
   ([name : String]
    [typ : syntax:type])
   #:transparent)
 
 (define-type syntax:type
   (U syntax:var))
-(struct syntax:var ([name : String]) #:transparent)
+(struct syntax:var syntax
+  ([name : String]) #:transparent)
