fbc356853710e960658472d169d4a55c8c37f48f
dannypsnl <dannypsnl@gmail.com>
2020-06-26 10:32:53 +0800

    ind: add more syntax

--- syntax.rkt
+++ syntax.rkt
@@ -2,7 +2,18 @@
 
 (struct syntax ([pos : srcloc]) #:transparent)
 
-(struct syntax:inductive syntax
+(define-type syntax:stmt
+  (U syntax:stmt:type-of
+     syntax:stmt:inductive))
+;;; (type-of Nat)
+(struct syntax:stmt:type-of syntax
+  ([term : syntax:term]) #:transparent)
+
+;;; inductive
+; (inductive Nat
+;   (z : Nat)
+;   (s : Nat -> Nat))
+(struct syntax:stmt:inductive syntax
   ([name : String]
    [constructors : (List syntax:constructor)])
   #:transparent)
@@ -11,7 +22,19 @@
    [typ : syntax:type])
   #:transparent)
 
+(define-type syntax:term
+  (U syntax:var
+     syntax:λ))
 (define-type syntax:type
   (U syntax:var))
+;;; x
 (struct syntax:var syntax
   ([name : String]) #:transparent)
+;;; t1 -> t2
+(struct syntax:arrow syntax
+  ([t1 : syntax:type]
+   [t2 : syntax:type]) #:transparent)
+(struct syntax:λ syntax
+  ([param-name : String]
+   [param-typ : syntax:type]
+   [body : syntax:term]) #:transparent)
