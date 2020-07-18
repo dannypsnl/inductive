2fc7eb8afe7a2ba18ff74a9a9752d6f390a894fb
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-19 03:16:15 +0800

    ind: cleanup and prepare to try nanopass

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "inductive")
-(define deps '("base"))
+(define deps '("base" "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Inductive")
 (define version "0.0")
