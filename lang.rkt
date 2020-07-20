cc907145df3500397e7c5dd27854be4642f54f55
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-07-20 12:19:56 +0800

    ind: apply construction

--- lang.rkt
+++ lang.rkt
@@ -17,34 +17,73 @@
    (variable (v c)))
   (Expr (e)
         v
+        (e0 e1)
         (inductive v (c* typ*) ...)))
 
-;;; improve t:ind, add more info for type checking
-(struct t:ind ())
+(struct t:ind (name constructor*) #:transparent)
+(struct t:construction
+  (name arg*) #:transparent)
+(define (: tm typ)
+  (unless (t:ind? typ) (error "unknown type"))
+  (unless (t:construction? tm) (error "unknown term"))
+  (ormap (λ (expected-c-name)
+           (eqv? (t:construction-name tm)
+                 expected-c-name))
+         (t:ind-constructor* typ)))
 
+(define (typ->construction name ty ctx)
+  (match ty
+    [`(-> ,t1 ,t2)
+     (λ (x)
+       (: x (hash-ref ctx t1))
+       (t:construction name (list x)))]
+    [t (t:construction name '())]))
+
+(define/match (t->string t)
+  [((t:construction name arg*))
+   (if (empty? arg*)
+       (symbol->string name)
+       (string-join (cons (symbol->string name) (map t->string arg*)) " "
+                    #:before-first "("
+                    #:after-last ")"))]
+  [(void) ""])
+
+(define (eval-list t* ctx)
+  (match t*
+    ['() (void)]
+    [(cons h t*)
+     (let ([e (eval h ctx)])
+       (displayln (t->string (car e)))
+       (eval-list t* (cdr e)))]))
 (define (eval t ctx)
   (nanopass-case (Inductive Expr) t
                  [(inductive ,v
                              (,c* ,typ*) ...)
-                  (let ([ctx (hash-set ctx v (t:ind))])
-                    (foldl (λ (c-name typ ctx)
-                             ;;; TODO: convert typ to term
-                             (hash-set ctx c-name typ))
-                           ctx
-                           c*
-                           typ*))]
-                 [else t]))
+                  (let ([ctx (foldl (λ (c-name c-typ ctx)
+                                      (hash-set ctx
+                                                c-name
+                                                (typ->construction c-name c-typ ctx)))
+                                    (hash-set ctx v (t:ind v
+                                                           c*))
+                                    c*
+                                    typ*)])
+                    (cons void ctx))]
+                 [(,e0 ,e1)
+                  (let* ([e0 (eval e0 ctx)]
+                         [e1 (eval e1 (cdr e0))])
+                    (cons ((car e0) (car e1)) (cdr e1)))]
+                 [,v
+                  (cons (hash-ref ctx v) ctx)]))
 
 (module+ test
   (define-parser ind-parser Inductive)
-  (ind-parser '(inductive Nat
-                          [z Nat]
-                          [s (-> Nat Nat)]))
-  (ind-parser 'z)
 
-  (define (eval-t t)
-    (eval (ind-parser t) (make-immutable-hash)))
+  (define (eval-t t*)
+    (eval-list (map (λ (t) (ind-parser t)) t*) (make-immutable-hash)))
 
-  (eval-t '(inductive Nat
-                      [z Nat]
-                      [s (-> Nat Nat)])))
+  (eval-t '((inductive Nat
+                       [z Nat]
+                       [s (-> Nat Nat)])
+            z
+            (s z)
+            (s (s z)))))
