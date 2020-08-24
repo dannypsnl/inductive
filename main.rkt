82169f0745d5d2fad9d4eca943a4618b695df5f6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-08-25 03:21:19 +0800

    ind: cleanup

--- main.rkt
+++ main.rkt
@@ -3,25 +3,16 @@
 (provide (except-out (all-from-out racket) #%module-begin #%top-interaction)
          (rename-out [module-begin #%module-begin]
                      [top-interaction #%top-interaction]))
-#|
-FIXME: add syntax wrapper to fix example
-NOTE
-* pretty every top level application
-* ind provide new inductive data type
-* provide match syntax wrapper
-* define-for-syntax?
-|#
+
 (module+ test
   (require rackunit))
-(require (for-syntax syntax/parse)
-         racket/syntax
-         syntax/stx)
+(require (for-syntax syntax/parse))
 (require "core.rkt")
 
 (define-syntax (top-interaction stx)
   (define-syntax-class typ
     (pattern t:id)
-    (pattern (t:id tp*:typ ...)))
+    (pattern (t:id tp*:typ)))
   (define-syntax-class constructor
     ; [z Nat]
     ; [s (n Nat) Nat]
@@ -44,7 +35,7 @@ NOTE
     [`((~literal ind) (name:id [tp*:id tptyp*:typ] ...) c*:constructor ...)
      #'(begin
          (define (name tp* ...)
-           (: tp* tptyp*) ...
+           (: tp* 'tptyp*) ...
            (tt (list 'name tp* ...) U))
          c*.def ...)]
     [`((~literal ind) . bad)
@@ -62,81 +53,89 @@ NOTE
 (module reader syntax/module-reader
   inductive)
 
-#;(module+ test
-    (define (List A)
-      (: A U)
-      (tt `(List ,A) U))
-    (define (nil) (tt 'nil (List (? U))))
-    (define (:: #:A [A (? U)] a lst)
-      (unify A (<- a))
-      (: A U)
-      (unify (List A) (<- lst))
-      (tt `(:: ,a ,lst) (List A)))
+(module+ test
+  (top-interaction
+   (ind Bool
+        [true Bool]
+        [false Bool]))
 
-    (define (Vec LEN E)
-      (: LEN Nat)
-      (: E U)
-      (tt `(Vec ,LEN ,E) U))
-    (define (vecnil) (tt 'vecnil (Vec (z) (? U))))
-    (define (vec:: #:E [E (? U)] #:LEN [LEN (? Nat)] e v)
-      (unify E (<- e))
-      (: E U)
-      (unify (Vec LEN E) (<- v))
-      (tt `(vec:: ,e ,v) (Vec (s LEN) E)))
+  (top-interaction
+   (ind Nat
+        [z Nat]
+        [s (n Nat) Nat]))
 
-    (define (≡ #:A [A (? U)] a b)
-      (: A U)
-      (unify (<- a) A)
-      (: b A)
-      (tt `(≡ ,A ,a ,b) U))
-    (define (refl #:A [A (? U)] #:a [a (? A)])
-      (tt 'refl (≡ a a)))
+  (define (List A)
+    (: A U)
+    (tt `(List ,A) U))
+  (define (nil) (tt 'nil (List (? U))))
+  (define (:: #:A [A (? U)] a lst)
+    (unify A (<- a))
+    (: A U)
+    (unify (List A) (<- lst))
+    (tt `(:: ,a ,lst) (List A)))
 
-    (check-equal? (pretty (s (s (z))))
-                  '(: (s (: (s (: z (: Nat U))) (: Nat U))) (: Nat U)))
-    (check-equal? (pretty (List Nat))
-                  '(: (List (: Nat U)) U))
-    (check-equal? (pretty (:: (s (z)) (:: (z) (nil))))
-                  '(:
-                    (::
-                     (: (s (: z (: Nat U))) (: Nat U))
-                     (:
-                      (:: (: z (: Nat U)) (: nil (: (List (: Nat U)) U)))
-                      (: (List (: Nat U)) U)))
+  (define (Vec LEN E)
+    (: LEN Nat)
+    (: E U)
+    (tt `(Vec ,LEN ,E) U))
+  (define (vecnil) (tt 'vecnil (Vec (z) (? U))))
+  (define (vec:: #:E [E (? U)] #:LEN [LEN (? Nat)] e v)
+    (unify E (<- e))
+    (: E U)
+    (unify (Vec LEN E) (<- v))
+    (tt `(vec:: ,e ,v) (Vec (s LEN) E)))
+
+  (define (≡ #:A [A (? U)] a b)
+    (: A U)
+    (unify (<- a) A)
+    (: b A)
+    (tt `(≡ ,A ,a ,b) U))
+  (define (refl #:A [A (? U)] #:a [a (? A)])
+    (tt 'refl (≡ a a)))
+
+  (check-equal? (pretty (List Nat))
+                '(: (List (: Nat U)) U))
+  (check-equal? (pretty (:: (s (z)) (:: (z) (nil))))
+                '(:
+                  (::
+                   (: (s (: z (: Nat U))) (: Nat U))
+                   (:
+                    (:: (: z (: Nat U)) (: nil (: (List (: Nat U)) U)))
                     (: (List (: Nat U)) U)))
-    (check-equal? (pretty (vec:: (z) (vecnil)))
-                  '(:
-                    (vec:: (: z (: Nat U)) (: vecnil (: (Vec (: z (: Nat U)) (: Nat U)) U)))
-                    (: (Vec (: (s (: z (: Nat U))) (: Nat U)) (: Nat U)) U)))
+                  (: (List (: Nat U)) U)))
+  (check-equal? (pretty (vec:: (z) (vecnil)))
+                '(:
+                  (vec:: (: z (: Nat U)) (: vecnil (: (Vec (: z (: Nat U)) (: Nat U)) U)))
+                  (: (Vec (: (s (: z (: Nat U))) (: Nat U)) (: Nat U)) U)))
 
-    (define (vec/length v)
-      (define LEN (? Nat))
-      (define E (? U))
-      (unify (Vec LEN E) (<- v))
-      LEN)
-    (check-equal? (pretty (vec/length (vec:: (z) (vec:: (z) (vecnil)))))
-                  '(: (s (: (s (: z (: Nat U))) (: Nat U))) (: Nat U)))
+  (define (vec/length v)
+    (define LEN (? Nat))
+    (define E (? U))
+    (unify (Vec LEN E) (<- v))
+    LEN)
+  (check-equal? (pretty (vec/length (vec:: (z) (vec:: (z) (vecnil)))))
+                '(: (s (: (s (: z (: Nat U))) (: Nat U))) (: Nat U)))
 
-    #;(define (sym #:A [A (? U)] #:x [x (? A)] #:y [y (? A)]
-                   [P1 (? (≡ x y))])
-        (unify (refl) P1)
-        (let ([r (refl)])
-          (unify (≡ y x) (<- r))
-          r))
-    #;(pretty (sym))
+  #;(define (sym #:A [A (? U)] #:x [x (? A)] #:y [y (? A)]
+                 [P1 (? (≡ x y))])
+      (unify (refl) P1)
+      (let ([r (refl)])
+        (unify (≡ y x) (<- r))
+        r))
+  #;(pretty (sym))
 
-    (define (Nat/+ m n)
-      (: m Nat)
-      (: n Nat)
-      (match (tt-tm (?/get m))
-        ['z n]
-        [`(s ,m-)
-         (s (Nat/+ m- n))]))
-    (check-equal? (pretty (Nat/+ (s (z)) (s (z))))
-                  '(: (s (: (s (: z (: Nat U))) (: Nat U))) (: Nat U)))
+  (define (Nat/+ m n)
+    (: m Nat)
+    (: n Nat)
+    (match (tt-tm (?/get m))
+      ['z n]
+      [`(s ,m-)
+       (s (Nat/+ m- n))]))
+  (check-equal? (pretty (Nat/+ (s (z)) (s (z))))
+                '(: (s (: (s (: z (: Nat U))) (: Nat U))) (: Nat U)))
 
-    #;(define (+0/Nat #:x [x (? Nat)])
-        (let ([r (refl)])
-          (unify (≡ (Nat/+ (z) x) x) (<- r))
-          r))
-    #;(pretty (+0/Nat)))
+  #;(define (+0/Nat #:x [x (? Nat)])
+      (let ([r (refl)])
+        (unify (≡ (Nat/+ (z) x) x) (<- r))
+        r))
+  #;(pretty (+0/Nat)))
